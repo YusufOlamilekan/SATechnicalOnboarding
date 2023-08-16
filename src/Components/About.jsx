704549fc69dcd3fc622d9f128d5f81bab2bc684e
Olamilekan Yusuf <horlamilekanyusuf@gmail.com>
2023-08-16 17:21:12 +0100

    ci: add Azure Static Web Apps workflow file on-behalf-of: @Azure opensource@microsoft.com

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,27 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/back1.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "Tech wave i/o";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm currently pursuing a BEng in Electrical and Electronics Engineering at the Federal University of Technology, Minna, Nigeria. ";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "IoT & Embedded Systems",
+  "Computer Vision",
+  "Electrical & Electronics Engineering",
+  "Circuit Design & Simulation",
+  "AI & Machine Learning",
+  "Community Lead",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am Passionate about creating innovative solutions through the integration of cutting-edge technologies, with a keen interest in embedded systems, IoT, computer vision, artificial intelligence, and machine learning.";
 
 const About = () => {
   return (
