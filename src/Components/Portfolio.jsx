5469cafd0e8d0b5bd410099c47c0e1655f44df38
Olamilekan Yusuf <horlamilekanyusuf@gmail.com>
2023-08-16 18:03:48 +0100

    COMMIT_EDITMSG

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/port.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "A future Tech screen";
 
 /**
  * Project list
@@ -31,7 +31,7 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Things To Know About Azure Static Web Apps 🎉",
     description:
       "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
     url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
